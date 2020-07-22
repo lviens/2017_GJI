6a8dea299a2decf540bb23e515403c6aa81a5f73
Loïc Viens <viens.loic.58r@st.kyoto-u.ac.jp>
2020-07-22 12:16:31 +0900

    Update Interferometry.py

--- Codes/Interferometry.py
+++ Codes/Interferometry.py
@@ -1,6 +1,6 @@
 # Script to compute deconvolution, cross-correlation, and coherency.
-# L. Viens: 04/28/2017
-# Code cleaned by L. Viens: 22/07/2020
+# Written by L. Viens (Initial release 04/28/2017)
+# Code cleaned by L. Viens (22/07/2020)
 
 import numpy as np
 from obspy import read
@@ -8,7 +8,7 @@ from obspy.signal.filter import bandpass
 import matplotlib.pyplot as plt
 from Functions_GJI_2017 import one_bit, cross_corr, deconvolution_stab, coherency_stab
 
-save_file =  "../Figures/foo.png" # Output file 
+save_file =  '../Figures/foo.png' # Output file 
 lag_dur = 100       # Duration of the signal to plot: between -100 to 100 s
 cut_per = .1        # Filter lower corner frequency (in Hz)
 cut_per2 = .5       # Filter higher corner frequency (in Hz)
@@ -18,7 +18,7 @@ stabilization = 10  # To stabilize the denominator of the deconvolution and cohe
 
 
 # Methods 
-methods = ["1-bit cross-corr" ,  "Cross-correlation" , "Coherency","Deconvolution"]
+methods = ['1-bit cross-corr',  'Cross-correlation', 'Coherency', 'Deconvolution']
 
 # Load virtual source and receiver data
 name_virt = '../Data/station_1_1d.sac' # Virtual source data (1 day)
@@ -33,13 +33,13 @@ delta = int(1/dt)
 # create time axis
 t = np.arange(-lag_dur, lag_dur, dt)
 
-# Split the 1 day data into "len_time_win" segments
+# Split the 1 day data into 'len_time_win' segments
 dat_s = np.split(dat_s1[0].data, len_time_win)
 dat_r = np.split(dat_r1[0].data, len_time_win)
 
 fig = plt.figure(figsize = (8, 10) )
 for ii in range(np.size(methods)): # Loop over the 4 methods
-    cnt = 1
+    cnt = 0
     sumfin = np.zeros( len(t))
     for i in range(len(dat_s)): # Loop over the data
         if methods[ii]=='1-bit cross-corr':
@@ -52,7 +52,8 @@ for ii in range(np.size(methods)): # Loop over the 4 methods
             corr, nodata, cnt = deconvolution_stab(dat_s[i], dat_r[i], delta, tpm =lag_dur, std_control = std_cont, count=cnt, stab=10)
         
         sumfin += corr # linear stack in time domain
-    
+    sumfin /= cnt # Divide by the number of waveforms
+
     # Band-pass filter the data with a 4-pole 2-pass Butterworth filter
     sumfin = bandpass(data = sumfin, freqmin = cut_per, freqmax = cut_per2, df = delta, corners = 4, zerophase = True)
     
@@ -72,6 +73,4 @@ for ii in range(np.size(methods)): # Loop over the 4 methods
 plt.tight_layout()
 plt.plot()
 fig.savefig(save_file, dpi=100)
-print("Figure saved as " + save_file)
-
-
+print('Figure saved as ' + save_file)
